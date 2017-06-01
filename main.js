f0abc2501825da64a54d2f2d58c2aa159b4b41de
foxneZz <foxnezz@gmail.com>
2017-06-01 15:49:52 +0300

    added ip logging in console

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 
 const app = express()
-const dataFile = "data.txt"
+const dataFile = "database/data.txt"
 
 const port = 80
 
@@ -11,8 +11,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'))
 
+function getIP(req)
+{
+    return req.ip.split(':').pop()
+}
+
 app.get('/', (req, res) =>
 {
+    console.log(`${getIP(req)} just accessed /`)
     fs.readFile(dataFile, (err, data) =>
     {
         res.render('index', {data: data.toString().split('\n').filter(a => a.length > 0)})
@@ -21,6 +27,7 @@ app.get('/', (req, res) =>
 
 app.post('/add', (req, res) =>
 {
+    console.log(`${getIP(req)} just accessed /add`)
     var newText = req.body.text
 
     if (newText.length == 0)
